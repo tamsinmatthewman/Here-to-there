5be48550f8172052ff329b4c1fa2a5c5f2dcbe88
tamsinmatthewman <66261762+tamsinmatthewman@users.noreply.github.com>
2023-09-30 21:44:43 -0700

    Add files via upload

--- script.js
+++ script.js
@@ -138,9 +138,9 @@ function resetRot() {
 
 if ("serviceWorker" in navigator) {
   window.addEventListener("load", function() {
-    navigator.serviceWorker
-      .register("/serviceWorker.js")
-      .then(res => console.log("service worker registered"))
-      .catch(err => console.log("service worker not registered", err))
-  })
+    navigator.serviceWorker;
+      .register("/serviceWorker.js");
+      .then(res => console.log("service worker registered"));
+      .catch(err => console.log("service worker not registered", err));
+  });
 }
--- serviceWorker.js
+++ serviceWorker.js
@@ -17,15 +17,15 @@ const assets = [
 self.addEventListener("install", installEvent => {
   installEvent.waitUntil(
     caches.open(staticGame).then(cache => {
-      cache.addAll(assets)
-    })
-  )
-})
+      cache.addAll(assets);
+    });
+  );
+});
 
 self.addEventListener("fetch", fetchEvent => {
   fetchEvent.respondWith(
     caches.match(fetchEvent.request).then(res => {
-      return res || fetch(fetchEvent.request)
-    })
-  )
-})
+      return res || fetch(fetchEvent.request);
+    });
+  );
+});
