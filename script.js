68041328efa346c6b2275372a02e78b395e77c4b
tamsinmatthewman <66261762+tamsinmatthewman@users.noreply.github.com>
2023-09-30 22:16:17 -0700

    Add files via upload

--- script.js
+++ script.js
@@ -139,7 +139,7 @@ function resetRot() {
 if ("serviceWorker" in navigator) {
   window.addEventListener("load", function() {
     navigator.serviceWorker
-      .register("/serviceWorker.js")
+      .register("serviceWorker.js")
       .then(res => console.log("service worker registered"))
       .catch(err => console.log("service worker not registered", err));
   });
